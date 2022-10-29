1e40449786292d3c436fba8e249b9c78017699b2
Aoyi Sim <57944276+dWKZ5jSZ8F@users.noreply.github.com>
2022-10-29 23:04:55 +0800

    Add files via upload

---
+++ hw2/main.cpp
@@ -0,0 +1,324 @@
+/* Sockets Example
+ * Copyright (c) 2016-2020 ARM Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "mbed.h"
+#include "wifi_helper.h"
+#include "mbed-trace/mbed_trace.h"
+#include "stm32l475e_iot01_tsensor.h"
+#include "stm32l475e_iot01_hsensor.h"
+#include "stm32l475e_iot01_psensor.h"
+#include "stm32l475e_iot01_magneto.h"
+#include "stm32l475e_iot01_gyro.h"
+#include "stm32l475e_iot01_accelero.h"
+
+#if MBED_CONF_APP_USE_TLS_SOCKET
+#include "root_ca_cert.h"
+
+#ifndef DEVICE_TRNG
+#error "mbed-os-example-tls-socket requires a device which supports TRNG"
+#endif
+#endif // MBED_CONF_APP_USE_TLS_SOCKET
+
+class SocketDemo {
+    static constexpr size_t MAX_NUMBER_OF_ACCESS_POINTS = 10;
+    static constexpr size_t MAX_MESSAGE_RECEIVED_LENGTH = 1024;
+
+#if MBED_CONF_APP_USE_TLS_SOCKET
+    static constexpr size_t REMOTE_PORT = 443; // tls port
+#else
+    static constexpr size_t REMOTE_PORT = 6969; // standard HTTP port
+#endif // MBED_CONF_APP_USE_TLS_SOCKET
+
+public:
+    SocketDemo() : _net(NetworkInterface::get_default_instance())
+    {
+    }
+
+    ~SocketDemo()
+    {
+        if (_net) {
+            _net->disconnect();
+        }
+    }
+
+    void run()
+    {
+        if (!_net) {
+            printf("Error! No network interface found.\r\n");
+            return;
+        }
+
+        /* if we're using a wifi interface run a quick scan */
+        if (_net->wifiInterface()) {
+            /* the scan is not required to connect and only serves to show visible access points */
+            wifi_scan();
+
+            /* in this example we use credentials configured at compile time which are used by
+             * NetworkInterface::connect() but it's possible to do this at runtime by using the
+             * WiFiInterface::connect() which takes these parameters as arguments */
+        }
+
+        /* connect will perform the action appropriate to the interface type to connect to the network */
+
+        printf("Connecting to the network...\r\n");
+
+        nsapi_size_or_error_t result = _net->connect();
+        if (result != 0) {
+            printf("Error! _net->connect() returned: %d\r\n", result);
+            return;
+        }
+
+        print_network_info();
+
+        /* opening the socket only allocates resources */
+        result = _socket.open(_net);
+        if (result != 0) {
+            printf("Error! _socket.open() returned: %d\r\n", result);
+            return;
+        }
+
+#if MBED_CONF_APP_USE_TLS_SOCKET
+        result = _socket.set_root_ca_cert(root_ca_cert);
+        if (result != NSAPI_ERROR_OK) {
+            printf("Error: _socket.set_root_ca_cert() returned %d\n", result);
+            return;
+        }
+        _socket.set_hostname(MBED_CONF_APP_HOSTNAME);
+#endif // MBED_CONF_APP_USE_TLS_SOCKET
+
+        /* now we have to find where to connect */
+
+        SocketAddress address;
+
+        if (!resolve_hostname(address)) {
+            return;
+        }
+
+        address.set_port(REMOTE_PORT);
+
+        /* we are connected to the network but since we're using a connection oriented
+         * protocol we still need to open a connection on the socket */
+
+        printf("Opening connection to remote port %d\r\n", REMOTE_PORT);
+
+        result = _socket.connect(address);
+        if (result != 0) {
+            printf("Error! _socket.connect() returned: %d\r\n", result);
+            return;
+        }
+
+        /* exchange an HTTP request and response */
+
+        if (!send_sensor_values()) {
+            return;
+        }
+
+        _socket.close();
+        printf("Data transmission completed.\n");
+        _net->disconnect();
+    }
+
+private:
+    bool resolve_hostname(SocketAddress &address)
+    {
+        const char hostname[] = MBED_CONF_APP_HOSTNAME;
+
+        /* get the host address */
+        printf("\nResolve hostname %s\r\n", hostname);
+        nsapi_size_or_error_t result = _net->gethostbyname(hostname, &address);
+        if (result != 0) {
+            printf("Error! gethostbyname(%s) returned: %d\r\n", hostname, result);
+            return false;
+        }
+
+        printf("%s address is %s\r\n", hostname, (address.get_ip_address() ? address.get_ip_address() : "None") );
+
+        return true;
+    }
+
+    bool send_http_request()
+    {
+        /* loop until whole request sent */
+        const char buffer[] = "GET / HTTP/1.1\r\n"
+                              "Host: ifconfig.io\r\n"
+                              "Connection: close\r\n"
+                              "\r\n";
+
+        nsapi_size_t bytes_to_send = strlen(buffer);
+        nsapi_size_or_error_t bytes_sent = 0;
+
+        printf("\r\nSending message: \r\n%s", buffer);
+
+        while (bytes_to_send) {
+            bytes_sent = _socket.send(buffer + bytes_sent, bytes_to_send);
+            if (bytes_sent < 0) {
+                printf("Error! _socket.send() returned: %d\r\n", bytes_sent);
+                return false;
+            } else {
+                printf("sent %d bytes\r\n", bytes_sent);
+            }
+
+            bytes_to_send -= bytes_sent;
+        }
+
+        printf("Complete message sent\r\n");
+
+        return true;
+    }
+
+    bool receive_http_response()
+    {
+        char buffer[MAX_MESSAGE_RECEIVED_LENGTH];
+        int remaining_bytes = MAX_MESSAGE_RECEIVED_LENGTH;
+        int received_bytes = 0;
+
+        /* loop until there is nothing received or we've ran out of buffer space */
+        nsapi_size_or_error_t result = remaining_bytes;
+        while (result > 0 && remaining_bytes > 0) {
+            result = _socket.recv(buffer + received_bytes, remaining_bytes);
+            if (result < 0) {
+                printf("Error! _socket.recv() returned: %d\r\n", result);
+                return false;
+            }
+
+            received_bytes += result;
+            remaining_bytes -= result;
+        }
+
+        /* the message is likely larger but we only want the HTTP response code */
+
+        printf("received %d bytes:\r\n%.*s\r\n\r\n", received_bytes, strstr(buffer, "\n") - buffer, buffer);
+
+        return true;
+    }
+
+    bool send_sensor_values(){
+        int data_number = 0;
+        int16_t pDataXYZ[3] = {0}, pMagDataXYZ[3] = {0};
+        float temperature, humidity, pressure, pGyroDataXYZ[3] = {0};
+        char acc_json[MAX_MESSAGE_RECEIVED_LENGTH];
+
+        printf("Start sensor init\n");
+        BSP_TSENSOR_Init();
+        BSP_HSENSOR_Init();
+        BSP_PSENSOR_Init();
+        BSP_MAGNETO_Init();
+        BSP_GYRO_Init();
+        BSP_ACCELERO_Init();
+
+        while(data_number < 25){
+            printf("Data: %d\n", data_number);
+            temperature = BSP_TSENSOR_ReadTemp();
+            humidity = BSP_HSENSOR_ReadHumidity();
+            pressure = BSP_PSENSOR_ReadPressure();
+            BSP_MAGNETO_GetXYZ(pMagDataXYZ);
+            BSP_GYRO_GetXYZ(pGyroDataXYZ);
+            BSP_ACCELERO_AccGetXYZ(pDataXYZ);
+            printf("TEMPERATURE = %.2f degC\n", temperature);
+            printf("HUMIDITY    = %.2f %%\n", humidity);
+            printf("PRESSURE is = %.2f mBar\n", pressure);
+            printf("MAG_X = %d\nMAG_Y = %d\nMAG_Z = %d\n", pMagDataXYZ[0], pMagDataXYZ[1], pMagDataXYZ[2]);
+            printf("GYRO_X = %.2f\nGYRO_Y = %.2f\nGYRO_Z = %.2f\n", pGyroDataXYZ[0], pGyroDataXYZ[1], pGyroDataXYZ[2]);
+            printf("ACCELERO_X = %d\nACCELERO_Y = %d\nACCELERO_Z = %d\n", pDataXYZ[0], pDataXYZ[1], pDataXYZ[2]);
+            int len = sprintf(acc_json, "%f %f %f %f %f %f %f %f %f %f %f %f %d",
+                        (float)((int)(temperature*10000))/10000,
+                        (float)((int)(humidity*10000))/10000,
+                        (float)((int)(pressure*10000))/10000,
+                        (float)((int)(pMagDataXYZ[0]*10000))/10000,
+                        (float)((int)(pMagDataXYZ[1]*10000))/10000,
+                        (float)((int)(pMagDataXYZ[2]*10000))/10000,
+                        (float)((int)(pGyroDataXYZ[0]*10000))/10000,
+                        (float)((int)(pGyroDataXYZ[1]*10000))/10000, 
+                        (float)((int)(pGyroDataXYZ[2]*10000))/10000, 
+                        (float)((int)(pDataXYZ[0]*10000))/10000,
+                        (float)((int)(pDataXYZ[1]*10000))/10000, 
+                        (float)((int)(pDataXYZ[2]*10000))/10000,
+                        data_number);
+            printf("%s\n",acc_json);
+            int response = _socket.send(acc_json, len);
+            if(response <= 0){
+                printf("Error! _socket.send returned: %d\n", response);
+            }
+            else{
+                printf("sent %d bytes\n\n", len);
+            }
+            data_number++;
+            ThisThread::sleep_for(1s);
+        }
+        return true;
+    }
+
+    void wifi_scan()
+    {
+        WiFiInterface *wifi = _net->wifiInterface();
+
+        WiFiAccessPoint ap[MAX_NUMBER_OF_ACCESS_POINTS];
+
+        /* scan call returns number of access points found */
+        int result = wifi->scan(ap, MAX_NUMBER_OF_ACCESS_POINTS);
+
+        if (result <= 0) {
+            printf("WiFiInterface::scan() failed with return value: %d\r\n", result);
+            return;
+        }
+
+        printf("%d networks available:\r\n", result);
+
+        for (int i = 0; i < result; i++) {
+            printf("Network: %s secured: %s BSSID: %hhX:%hhX:%hhX:%hhx:%hhx:%hhx RSSI: %hhd Ch: %hhd\r\n",
+                   ap[i].get_ssid(), get_security_string(ap[i].get_security()),
+                   ap[i].get_bssid()[0], ap[i].get_bssid()[1], ap[i].get_bssid()[2],
+                   ap[i].get_bssid()[3], ap[i].get_bssid()[4], ap[i].get_bssid()[5],
+                   ap[i].get_rssi(), ap[i].get_channel());
+        }
+        printf("\r\n");
+    }
+
+    void print_network_info()
+    {
+        /* print the network info */
+        SocketAddress a;
+        _net->get_ip_address(&a);
+        printf("IP address: %s\r\n", a.get_ip_address() ? a.get_ip_address() : "None");
+        _net->get_netmask(&a);
+        printf("Netmask: %s\r\n", a.get_ip_address() ? a.get_ip_address() : "None");
+        _net->get_gateway(&a);
+        printf("Gateway: %s\r\n", a.get_ip_address() ? a.get_ip_address() : "None");
+    }
+
+private:
+    NetworkInterface *_net;
+
+#if MBED_CONF_APP_USE_TLS_SOCKET
+    TLSSocket _socket;
+#else
+    TCPSocket _socket;
+#endif // MBED_CONF_APP_USE_TLS_SOCKET
+};
+
+int main() {
+    printf("\r\nStarting socket demo\r\n\r\n");
+
+#ifdef MBED_CONF_MBED_TRACE_ENABLE
+    mbed_trace_init();
+#endif
+
+    SocketDemo *example = new SocketDemo();
+    MBED_ASSERT(example);
+    example->run();
+
+    return 0;
+}
